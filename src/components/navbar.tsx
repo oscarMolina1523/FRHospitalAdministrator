2bc7a77f35bff4049441f5b8381e41757198a675
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-11-01 17:39:00 -0600

    feat: initial configurations

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <div>
+      <div>navbar</div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/layouts/MainLayout.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MainLayout: React.FC = () => {
+  return (
+    <div>
+      <div>main layout</div>
+    </div>
+  );
+};
+
+export default MainLayout;
