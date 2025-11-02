cdb1fbd61c7c13d528b6b4e90339796fd789d33d
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-11-01 22:01:54 -0600

    style: add padding in outlet

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -5,13 +5,13 @@ import { Outlet } from "react-router-dom";
 
 export const MainLayout: React.FC = () => {
   return (
-    <div className="w-full h-full flex flex-col gap-2">
+    <div className="w-full h-full flex flex-col gap-4">
       <Navbar />
       <div className="w-full flex flex-row h-full">
-        <div className="w-1/4 p-2">
+        <div className="w-1/4 px-2">
           <Sidebar />
         </div>
-        <div className="bg-red-400"><Outlet /></div>
+        <div className="w-3/4 px-2"><Outlet /></div>
       </div>
     </div>
   );
