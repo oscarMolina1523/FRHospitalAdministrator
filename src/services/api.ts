007907622a541aa7ba8b507776de23176af4a523
oscarMolina1523 <oscarmolina1523@gmail.com>
2025-11-11 18:54:41 -0600

    feat: add api url of vercel backend project

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL="https://hospital-1cq9.onrender.com"
+export const API_BASE_URL="https://api-hospital-ni.vercel.app"
+//export const API_BASE_URL="https://hospital-1cq9.onrender.com" //render api
